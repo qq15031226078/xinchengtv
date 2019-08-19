2f64c7dc6914894f4b1acdfa9d835f76fa7218b4
mac <mac@macdeMacBook-Pro.local>
2019-08-19 14:36:41 +0800

    up

--- app/Models/ProgramNewsRef.php
+++
@@ -1,9 +0,0 @@
-<?php
- namespace App\Models;
-
- use Illuminate\Support\Facades\DB;
- class ProgramNewsRef extends BaseModel
- {
-     protected $table = 'xc_program_news_ref';
- 
- }
--- resources/lang/en/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'incorrect' => 'email or password is incorrect',
-];
